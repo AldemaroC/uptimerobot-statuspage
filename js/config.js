abce52410acb92c10e95c6fd78b8613447167590
Aldemaro Campos <marocampos94@gmail.com>
2019-09-29 15:28:28 -0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,11 @@
+var __apiKeys = [
+	'm783538923-fd83f5bc256273d16adddf87', //dccge01
+	'm783538924-bd2d5b5e297a8e6c6b4bc28a', //dcspo01
+	'm783538929-bcf69a4e55382e5f52cd46f6', //dcspo02
+	'm783538931-81b61a56cad1a8deb3526e5b', //dccan01
+	'm780377589-d3e2b1260ca311aa0e8bab43', //site
+	'm780377598-0fdfe29753c9fbc39c99e1c9', //cliente
+	'm780377600-79c043fb868b7bc4708cca41', //cloud
+	'm780971172-d30a2bb5b7a2fbed06ddd7ab', //painel
+];
+var __refresh = 180;
